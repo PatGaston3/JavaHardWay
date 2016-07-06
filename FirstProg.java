aa95b26c60129f8844f450bc282db3b9d00daa9e
Levi Gaston (Pat G) <Pat-Levi-Gaston@Patricks-MacBook-Pro.local>
2016-07-06 16:07:53 -0600

    add exercise 1

---
+++ FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg {
+	public static void main( String[] args ) {
+		System.out.println( "I, Patrick Gaston, am determined to learn how to code. " );
+		System.out.println( "Today's date is Wednesday, July 6th, 2016" );
+	}
+}
